bb478a30d4974aadd6c78379cbb11448581e45c4
Glêsio Santos <glesioss@gmail.com>
2023-12-26 09:49:19 -0300

    override express add user id

--- src/modules/auth/controllers/account_controller.ts
+++ src/modules/auth/controllers/account_controller.ts
@@ -2,9 +2,15 @@ import { Request, Response } from 'express'
 import { AddAccountService } from '../services/add_account_service'
 
 export class AccountController {
-    async addAccount(request: Request, response: Response): Promise<Response> {
-      const accountService = new AddAccountService()
-      await accountService.add(request.body)
-      return response.status(201).end()
-    }
+  async addAccount(request: Request, response: Response): Promise<Response> {
+    const accountService = new AddAccountService()
+    await accountService.add(request.body)
+    return response.status(201).end()
+  }
+
+  async loadAccount(request: Request, response: Response): Promise<Response> {
+    const accountService = new AddAccountService()
+    await accountService.add(request.body)
+    return response.status(201).end()
+  }
 }
--- src/shared/http/middlewares/is_authenticated.ts
+++ src/shared/http/middlewares/is_authenticated.ts
@@ -3,6 +3,10 @@ import AppError from '@shared/errors/app_error'
 import { NextFunction, Request, Response } from 'express'
 import authConfig from '@config/auth_config'
 
+export type TokenPayload = {
+  sub: string
+}
+
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
   const authHeader = request.headers.authorization
 
@@ -12,7 +16,11 @@ export default function isAuthenticated(request: Request, response: Response, ne
 
   try {
     const decode = jwt.verify(token, authConfig.jwt.secret)
-    console.log(decode)
+    const { sub } = decode
+
+    request.user = {
+      id: sub as string
+    }
   } catch (error) {
     throw new AppError('Invalid JWT Token', 401)
   }
