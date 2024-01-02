1cae4aae5c0bc86c74ed4ea0bc421fc34d56aa75
Glêsio Santos <glesioss@gmail.com>
2024-01-02 13:12:31 -0300

    upload avatar to account

--- src/config/app.ts
+++ src/config/app.ts
@@ -19,7 +19,7 @@ app.use((error: Error, request: Request, response: Response, next: NextFunction)
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ status: 'error', message: error.message })
   }
-
+  console.log(error.message)
   return response.status(500).json({ status: 'error', message: 'Internal Server Error' })
 })
 
--- src/modules/auth/services/update_avatar_account_service.ts
+++ src/modules/auth/services/update_avatar_account_service.ts
@@ -13,24 +13,29 @@ type AvatarAccountModel = {
 export class UpdateAvatarAccountService {
   async uploadImage(data: AvatarAccountModel): Promise<void> {
     const accountRepository = AppDataSource.getRepository(AccountModel)
-    const account: any = accountRepository.findOneBy({ id: data.id })
+    const account: any = await accountRepository.findOneBy({ id: data.id })
+    console.log('email: ' + account.email)
+    console.log('file: ' + data.avatar)
 
     if (!account) throw new AppError('Account not found')
 
-    if (account.avatar) {
+    if (account.avatar && account.avatar === 'avatar.default') {
       const avatarAccount = path.join(uploadConfig.directory, account.avatar)
       const avatarAccountExists = await fs.promises.stat(avatarAccount)
 
       if (avatarAccountExists) {
+        console.log('avatar', account.avatar)
         await fs.promises.unlink(avatarAccount) // apagar o arquivo
       }
 
+      console.log('arquivo ', data.avatar)
       await accountRepository
         .createQueryBuilder()
         .update(AccountModel)
         .set({
-          avatar: data.id
-        }).where({ id: data.id }).execute()
+          avatar: data.avatar
+          // updatedAt: new Date()
+        }).where({ id: account.id }).execute()
     }
   }
 }
