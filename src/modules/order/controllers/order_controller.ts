42095614da5cc18633810d0aa0b40d46a3bbbe73
Glêsio Santos <glesioss@gmail.com>
2024-01-14 20:07:39 -0300

    remove order and return quantity to stock

--- src/modules/order/controllers/order_controller.ts
+++ src/modules/order/controllers/order_controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from 'express'
 import { AddOrderService } from '../service/add_order_service'
 import { LoadAllOrderService } from '../service/load_all_order_service'
 import { LoadOrderByCustomerService } from '../service/load_order_by_customer'
+import { RemoveOrderService } from '../service/remove_order_service'
 
 export class OrderController {
   async saveOrder(request: Request, response: Response): Promise<Response> {
     const addOrderService = new AddOrderService()
     await addOrderService.add(request.body)
     return response.status(201).end()
+  }
+
+  async deleteOrders(request: Request, response: Response): Promise<Response> {
+    const removeOrderService = new RemoveOrderService()
+    console.log(request.params.id)
+    await removeOrderService.removeOrder(request.params.id)
+    return response.status(204).end()
   }
 
   async loadAllOrders(request: Request, response: Response): Promise<Response> {
--- src/shared/http/routers/order_route.ts
+++ src/shared/http/routers/order_route.ts
@@ -1,19 +1,28 @@
-import { OrderController } from '@modules/order/controllers/order_controller'
 import { Router } from 'express'
+import { OrderController } from '@modules/order/controllers/order_controller'
+import isAuthenticated from '../middlewares/is_authenticated'
 
 const orderController = new OrderController()
 
 export default (router: Router): void => {
   router.post(
     '/orders',
+    isAuthenticated,
     orderController.saveOrder
+  )
+  router.delete(
+    '/orders/:id',
+    isAuthenticated,
+    orderController.deleteOrders
   )
   router.get(
     '/orders',
+    isAuthenticated,
     orderController.loadAllOrders
   )
   router.post(
     '/orders/customer',
+    isAuthenticated,
     orderController.loadOrderByCustomer
   )
 }
