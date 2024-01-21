2a1c387efcd4e5b9361c6db1c96fde945a9d22fc
Glêsio Santos <glesioss@gmail.com>
2024-01-21 09:16:25 -0300

    update .env

--- src/config/auth_config.ts
+++ src/config/auth_config.ts
@@ -1,6 +1,8 @@
+import env from './env'
+
 export default {
   jwt: {
-    secret: 'accoe56d80c09aef1a6e098ea37fc7e63e79ec63c86a0d6b30069f8fe94f71291023unt',
-    expiresIn: '1d'
+    secret: env.jwtSecret,
+    expiresIn: env.expiresInJwt
   }
 }
