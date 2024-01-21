c89c0fd5b73bb60cf7c379cecd5a341ab9bfd1bf
Glêsio Santos <glesioss@gmail.com>
2024-01-20 22:53:20 -0300

    add admin_route and customer_route

--- src/shared/http/routers/customer_route.ts
+++ src/shared/http/routers/customer_route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
 import { CustomerController } from '@modules/customer/controllers/customer_controller'
+import isAuthenticated from '../middlewares/is_authenticated'
 
 const customerController = new CustomerController()
 
 export default (router: Router): void => {
   router.post(
     '/customers/add',
+    isAuthenticated,
     celebrate({
       [Segments.BODY]: {
         name: Joi.string().required(),
@@ -17,14 +19,17 @@ export default (router: Router): void => {
   )
   router.get(
     '/customers',
+    isAuthenticated,
     customerController.loadCustomer
   )
   router.get(
     '/customers/:phone',
+    isAuthenticated,
     customerController.loadCustomerByPhone
   )
   router.get(
     '/customers/order',
+    isAuthenticated,
     customerController.loadCustomerByPhone
   )
 }
