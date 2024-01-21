c3582982309e02467a808632b15b6d00fb337251
Glêsio Santos <116793448+glesiosantos@users.noreply.github.com>
2024-01-21 09:06:23 -0300

    add pagination to customer (#12)

--- src/modules/customer/services/load_customer_service.ts
+++ src/modules/customer/services/load_customer_service.ts
@@ -4,7 +4,8 @@ import { Customer } from '../models/customer'
 export class LoadCustomerService {
   async load(): Promise<Customer[]> {
     const customerRepository = AppDataSource.getRepository(Customer)
-    const customers = await customerRepository.find({})
+    // const customers = await customerRepository.find({})
+    const customers = await customerRepository.createQueryBuilder('customers').take(20).getMany()
     return customers ?? null
   }
 }
