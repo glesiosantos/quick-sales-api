74a74056c7fe40be8d4cd3a2d2b4b3aba88bd480
Glêsio Santos <116793448+glesiosantos@users.noreply.github.com>
2024-01-21 12:05:32 -0300

    Add Redis (#13)
    * update .env
    
    * implement redis to cache

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,12 +8,21 @@ export default {
   requireTLSEmail: process.env.REQUIRE_TLS_EMAIL,
   authUserEmail: process.env.AUTH_USER_EMAIL as string,
   authUserPasswd: process.env.AUTH_USER_PASSWD as string,
-  emailDefaultBySend: 'support@quicksales.com.br',
+  emailDefaultBySend: process.env.EMAILDEFAULTSUPPORT,
 
   // setting db
   hostDb: process.env.HOSTDB,
   portDb: process.env.PORTDB as number | undefined,
   usernameDb: process.env.USERDB,
   passwdDb: process.env.PASSWDDB,
-  dbName: process.env.DBNAME
+  dbName: process.env.DBNAME,
+
+  // JWT SETTINGS
+  jwtSecret: process.env.JWTSECRET,
+  expiresInJwt: process.env.EXPIRESINJWT,
+
+  // redis
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  redisPass: process.env.REDIS_PASS || undefined
 }
--- src/modules/product/services/load_all_product_service.ts
+++ src/modules/product/services/load_all_product_service.ts
@@ -1,10 +1,19 @@
 import { AppDataSource } from '@config/app_data_source'
 import ProductModel from '../models/product'
+import RedisCache from '@shared/cache/redis_cache'
 
 export class LoadAllProductService {
   async loadAll(): Promise<ProductModel[] | null> {
     const productRepository = AppDataSource.getRepository(ProductModel)
-    const products = await productRepository.find()
+
+    const redisCacheProduct = new RedisCache()
+    let products = await redisCacheProduct.recoverCache<ProductModel[]>('API_QS_PRODUCTS')
+
+    if (!products) {
+      products = await productRepository.find({})
+      await redisCacheProduct.saveCache('API_QS_PRODUCTS', products)
+    }
+
     return products
   }
 }
